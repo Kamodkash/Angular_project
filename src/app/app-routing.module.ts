f39b0b1a0ae058ea39d1e3b3f60586fa9723c5d5
kamod <kamod4113@gmail.com>
2023-10-27 19:23:48 +0530

    Added angular app startup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SupportComponent } from './support/support.component';
+import { ContactComponent } from './contact/contact.component';
+import { EmployeeComponent } from './employee/employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"home",pathMatch:"full"},
+  {path:"home",component:HomeComponent},
+  {path:"about",component:AboutComponent},
+  {path:"support",component:SupportComponent},
+  {path:"contact",component:ContactComponent},
+  {path:"employee",component:EmployeeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
