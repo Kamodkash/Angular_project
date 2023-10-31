5619fcd396627e653171e39711815787c49cd910
kamod <kamod4113@gmail.com>
2023-10-31 19:28:41 +0530

    created employee constructor

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { EmployeeService } from '../employee.service';
+import { Employee } from '../employee';
 
 @Component({
   selector: 'app-employee',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee.component.scss']
 })
 export class EmployeeComponent {
+  employeesList:Employee[]=[];
+  newEmployee:Employee = new Employee();
+  editEmployee:Employee = new Employee();
 
+  constructor(private employeeService:EmployeeService)
+  {
+  }
+  ngOnInit(){
+    this.getAll();
+  }
+  getAll()
+  {
+    this.employeeService.getAllEmployees().subscribe(
+      (response)=>{
+        this.employeesList = response;
+        console.log(this.employeesList);
+      },
+      (error)=>{
+        console.log(error);
+      }
+    );
+  }
 }
