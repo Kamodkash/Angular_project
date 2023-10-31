e72482732d7496a04de93491a070be4eee4b1ce6
kamod <kamod4113@gmail.com>
2023-10-31 19:27:02 +0530

    Created class employee

---
+++ src/app/employee.ts
@@ -0,0 +1,12 @@
+export class Employee {
+  id:number;
+  name:String;
+  address:String;
+  salary:number;
+  constructor(){
+    this.id= 0;
+    this.name= "";
+    this.address= "";
+    this.salary= 0;
+  }
+}
